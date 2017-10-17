edfd70c3febf14ee8062c234395056f598a7a1ab
L00Cyph3r <L00Cyph3r@users.noreply.github.com>
2017-10-17 14:49:29 +0200

    Better bootstrap and only latest wordpress

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,32 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $_tests_dir = getenv('WP_TESTS_DIR');
-if (!$_tests_dir) {
+if ($_tests_dir === false) {
   $_tests_dir = '/tmp/wordpress-tests-lib';
 }
-require_once $_tests_dir . '/includes/functions.php';
-require_once __DIR__ . '/../vendor/autoload.php';
-function _manually_load_plugin() {
-  require __DIR__ . '/../WPTools.php';
-}
 
-tests_add_filter('muplugins_loaded', '_manually_load_plugin');
+define('PLUGIN_FILE', getenv('PLUGIN_FILE'));
+define('PLUGIN_FOLDER', basename(dirname(__DIR__)));
+define('PLUGIN_PATH', PLUGIN_FOLDER . '/' . PLUGIN_FILE);
+
+$GLOBALS['wp_tests_options'] = [
+  'active_plugins' => [PLUGIN_PATH],
+  'template'       => 'twentysixteen',
+  'stylesheet'     => 'twentysixteen',
+];
+
+require_once $_tests_dir . '/includes/functions.php';
+
+tests_add_filter(
+  'muplugins_loaded',
+  function () {
+    // Manually load plugin
+    include __DIR__ . '/../WPTools.php';
+  }
+);
+
+tests_add_filter('shutdown', 'drop_tables', 999999);
+
 require $_tests_dir . '/includes/bootstrap.php';
