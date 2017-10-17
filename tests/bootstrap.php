b956e1f76b42a34c3924baf90fe253527f1898f0
L00Cyph3r <L00Cyph3r@users.noreply.github.com>
2017-10-17 14:11:18 +0200

    I am oetlul

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ if (!$_tests_dir) {
 require_once $_tests_dir . '/includes/functions.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 function _manually_load_plugin() {
-  require __DIR__ . '/../elvis-media.php';
+  require __DIR__ . '/../WPTools.php';
 }
 
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
