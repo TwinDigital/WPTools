fd8e58ebeea44a772a2acab608af213a618d69d9
L00Cyph3r <L00Cyph3r@users.noreply.github.com>
2017-10-17 17:34:12 +0200

    No zip needed

--- bin/install_gitlab_tests.sh
+++ bin/install_gitlab_tests.sh
@@ -38,20 +38,13 @@ install_wp() {
 		return;
 	fi
 	mkdir -p $WP_CORE_DIR
-	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-		mkdir -p /tmp/wordpress-nightly
-		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
-		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
-		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
-	else
-		if [ $WP_VERSION == 'latest' ]; then
-			local ARCHIVE_NAME='latest'
-		else
-			local ARCHIVE_NAME="wordpress-$WP_VERSION"
-		fi
-		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
-		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
-	fi
+  if [ $WP_VERSION == 'latest' ]; then
+    local ARCHIVE_NAME='latest'
+  else
+    local ARCHIVE_NAME="wordpress-$WP_VERSION"
+  fi
+  download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+  tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
 }
 install_test_suite() {
@@ -85,7 +78,7 @@ install_gitlab_test_runner() {
 	apt-get clean
 	apt-get -yqq update
 	# instll the required packages for the running CI tests
-	apt-get -yqqf install zip unzip subversion mysql-client libmysqlclient-dev --fix-missing
+	apt-get -yqqf install subversion mysql-client libmysqlclient-dev --fix-missing
 	# PHP extensions
 	docker-php-ext-enable mcrypt mysqli pdo_mysql intl gd
 }
