fd00e2001798744e9ee09e2f4c6d72244354bfdc
L00Cyph3r <L00Cyph3r@users.noreply.github.com>
2017-10-18 11:27:41 +0200

    Add test for: PluginTools::getPluginByTitle()

--- tests/PluginToolsTest.php
+++ tests/PluginToolsTest.php
@@ -37,4 +37,16 @@ class PluginToolsTest extends TestCase {
     PluginTools::refreshLoadedPlugins();
     $this->assertNotCount(0, PluginTools::$loadedPlugins, 'Pluginlist is empty, probably failed loading the list of plugins.');
   }
+
+  /**
+   * Tests if the plugin_list is loaded.
+   *
+   * @covers \TwinDigital\WPTools\PluginTools::getPluginByTitle()
+   * @return void
+   */
+  public function testGetPluginByTitle() {
+    PluginTools::refreshLoadedPlugins();
+    $this->assertEmpty(PluginTools::getPluginByTitle('Non-existing-plugin', 'Found a plugin that is non-existing? Oops'));
+    $this->assertNotEmpty(PluginTools::getPluginByTitle('WPTools', 'Current plugin is not active?'));
+  }
 }
