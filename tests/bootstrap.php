6aa0abb8143cc83c09ab13f5e1b776fcb178a6ac
L00Cyph3r <L00Cyph3r@users.noreply.github.com>
2017-11-02 10:14:12 +0100

    Change bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,27 @@
 <?php
-
 $_tests_dir = getenv('WP_TESTS_DIR');
+$_tests_dir = '/home/lucien/svn/wordpress-dev/trunk/tests/phpunit';
+if (is_dir($_tests_dir) !== true) {
+  $_tests_dir = false;
+}
 if ($_tests_dir === false) {
   $_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 define('PLUGIN_FILE', getenv('PLUGIN_FILE'));
-define('PLUGIN_FOLDER', __DIR__ . '/../');
+define('PLUGIN_BASEDIR', dirname(__DIR__), 2);
+define('PLUGIN_FOLDER', dirname(__DIR__));
 define('PLUGIN_PATH', PLUGIN_FOLDER . '/' . PLUGIN_FILE);
+
+require_once $_tests_dir . '/includes/functions.php';
 
 if (!@include PLUGIN_FOLDER . '/vendor/autoload.php') {
   if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:
+    die(
+    'You must set up the project dependencies, run the following commands:
         wget http://getcomposer.org/composer.phar
-        php composer.phar install');
+        php composer.phar install'
+    );
   }
 }
 
@@ -23,16 +31,14 @@ $GLOBALS['wp_tests_options'] = [
   'stylesheet'     => 'twentysixteen',
 ];
 
-require_once $_tests_dir . '/includes/functions.php';
+//tests_add_filter(
+//  'muplugins_loaded',
+//  function () {
+//    // Manually load plugin
+//    include_once PLUGIN_PATH;
+//  }
+//);
 
-tests_add_filter(
-  'muplugins_loaded',
-  function () {
-    // Manually load plugin
-    include_once PLUGIN_PATH;
-  }
-);
-
-tests_add_filter('shutdown', 'drop_tables', 999999);
+//tests_add_filter('shutdown', 'drop_tables', 999999);
 
 require $_tests_dir . '/includes/bootstrap.php';
